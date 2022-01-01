c373061493575a23e341e787661e850801b2c77a
ishan agrawal <ishan.agrawal90@gmail.com>
2022-01-02 00:38:05 +0800

    update deployed contract address

--- truffle-config.js
+++ truffle-config.js
@@ -65,10 +65,7 @@ module.exports = {
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, process.env.INFURA_URL),
       network_id: 4,
-      gas: 5500000,
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      gas: 5500000
     },
     // ropsten: {
     //   provider: () => new HDWalletProvider(mnemonic, process.env.INFURA_URL),
